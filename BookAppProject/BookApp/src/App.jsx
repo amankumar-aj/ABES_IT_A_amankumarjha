85e533146519abcacd794d88e6f1128834d4105c
amankumar-aj <amankumar_aj@outlook.com>
2025-11-03 12:49:42 +0530

    BOOK App created inital commit

---
+++ BookAppProject/BookApp/src/App.jsx
@@ -0,0 +1,14 @@
+import Book from './Components/Book.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Book />
+    </>
+  )
+}
+
+export default App
---
+++ BookAppProject/BookApp/src/Components/Book.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './book.css'
+
+function Book() {
+  return (
+    <div id='book'>
+        <img src="" alt="" />
+        <h1>Maths</h1>
+        <p>by Amankumar Jha</p>
+        <span>Price: $10</span>
+
+    </div>
+  )
+}
+
+export default Book
